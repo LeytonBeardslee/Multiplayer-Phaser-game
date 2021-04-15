ef43960cdbe8b2bb366306e0e009631f7ba6a65b
Leyton Beardslee <leytonbeardslee@gmail.com>
2021-04-15 10:10:17 -0400

    first push

--- src/client/actors/player/player.class.ts
+++ src/client/actors/player/player.class.ts
@@ -1,35 +1,64 @@
-export class Player {
-    public player: Phaser.Sprite;
-    public controls: KeyBoardControl;
-    public playerState: Map<string, boolean | number>;
-    public angularVelocity: number = 300;
-    constructor(private gameInstance: Phaser.Game, public playerInstance: any) {
-            this.createPlayer(this.gameInstance);
-            this.playerInstance = playerInstance; 
-            this.playerState = new Map();
+    export class Player {
+        public player: Phaser.Sprite;
+        public projectile: Projectile;
+        public controls: KeyBoardControl;
+        public playerState: Map<string, boolean | number>;
+        public angularVelocity: number = 300;
+        constructor(private gameInstance: any, public
+        playerInstance: any) {
+        this.createPlayer(this.gameInstance);
+        this.playerState = new Map();
         }
-        public createPlayer(): void {
-
+        public createPlayer(gameInstance): void {
+        this.addControls();
+        this.player = gameInstance.add.sprite(100,100,'shooter-sprite');
+        this.player.id = "1";
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.animations.add('accelerating',
+        
+        [1, 0], 60, false);
+        
+        this.player.name = "your name";
+        this.attachPhysics(gameInstance);
         }
-        public assignPickup(): void {
-
+        Chapter 4 Implementing Our Game Domain Models
+        
+        39
+        
+        public assignPickup(game, player?): void {
+        this.projectile = new Projectile(game, player.
+        
+        player);
+        
+        this.playerState.set('ammo', this.projectile.
+        
+        bulletCount);
+        
         }
         public view(): void {
-
+        this.controls.update();
         }
         private addControls(): void {
-
+        this.controls = new KeyBoardControl(this.gameInstance, this);
+        
         }
-        private attachPhysics(): void {
-
+        private attachPhysics(gameInstance): void {
+        gameInstance.physics.enable(this.player, Phaser.
+        
+        Physics.ARCADE);
+        
+        this.player.body.collideWorldBounds = true;
+        this.player.body.bounce.setTo(10, 10);
+        this.player.body.gravity.y = 0;
+        this.player.body.drag.set(80);
+        this.player.body.maxVelocity.set(100);
+        this.player.body.immovable = false;
         }
-    }
-    public createPlayer(gameInstance): void {
-        this.addControls();
-        this.player = gameInstance.add.sprite(
-            100, 100, 'shooter-sprite'
-        );
-        this.player.anchor.setTo(0.5, 0.5);
-        this.attachPhysics(gameInstance);
-    }
+        }
+        
+        Chapter 4 Implementing Our Game Domain Models
+        
+        40
+        The Game Model
+        Our little spaceship needs a world
     
